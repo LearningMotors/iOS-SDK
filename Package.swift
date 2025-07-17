8f4f478f0a3cbbb2f7c156dec57df7f0c21d91d9
Naveen Katari <nkatari@suki.ai>
2025-07-18 00:43:04 +0530

    Add Package.swift for SukiAmbientCore v2.3.0

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SukiAmbientCore",
+    platforms: [
+        .iOS("15.0")
+    ],
+    products: [
+        .library(
+            name: "SukiAmbientCore",
+            targets: ["SukiAmbientCore"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SukiAmbientCore",
+            url: "https://github.com/LearningMotors/iOS-Framework/releases/download/2.3.0/SukiAmbientCore.xcframework.zip",
+            checksum: "147aaa09fb27d30d79b5918382769a1e8b38c61c8eec5349da9976ef0d975958"
+        )
+    ]
+)
