0f25df89977fed3abd9fd8f6d6d5e70b5f62d580
Naveen Katari <nkatari@suki.ai>
2025-08-12 10:22:39 +0530

    feat: bundle xcframework and switch to path-based target; add .gitattributes

--- Package.swift
+++ Package.swift
@@ -3,14 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "SukiAmbientCore",
-    platforms: [.iOS("15.0")],
-    products: [.library(name: "SukiAmbientCore", targets: ["SukiAmbientCore"])],
+    platforms: [ .iOS("15.0") ],
+    products: [ .library(name: "SukiAmbientCore", targets: ["SukiAmbientCore"]) ],
     targets: [
         .binaryTarget(
             name: "SukiAmbientCore",
-            url: "https://github.com/LearningMotors/iOS-SDK/releases/download/2.4.0/SukiAmbientCore.xcframework.zip",
-
-            checksum: "57a5c6661cff41a603e050897de978e0c783793c19c5134f6c316f2d41bf3ceb"
+            path: "SukiAmbientCore.xcframework"
         )
     ]
 )
