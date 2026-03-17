708c0422294fa54bc68ac8eee330f3cb4d0f8616
Naveen Katari <nkatari@suki.ai>
2026-03-17 19:42:51 +0530

    Update binary target to release 2.5.4

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SukiAmbientCore",
-            url: "https://github.com/LearningMotors/iOS-SDK/releases/download/2.5.2/SukiAmbientCore.xcframework.zip",
-            checksum: "9b8cf3069796bd148dbafe649f8fa514968a3cc8b7a6b0434c650935e05f92d7"
+            url: "https://github.com/LearningMotors/iOS-SDK/releases/download/2.5.4/SukiAmbientCore.xcframework.zip",
+            checksum: "acd0a58f5b848cae1413c99182942b7cc7114d340c0e384cfd7d8d4a39ac5a95"
         )
     ]
 )
